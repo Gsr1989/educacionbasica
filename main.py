3bffaab344fe23ac6000b39133257cf01bd2ddde
Gsr1989 <empresalorens@gmail.com>
2025-04-24 21:11:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
+from supabase import create_client, Client
+
 app = Flask(__name__)
+
+# Conexión a Supabase
+SUPABASE_URL = "https://axgqvhgtbzkraytzaomw.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Z3F2aGd0YnprcmF5dHphb213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NDAwNzUsImV4cCI6MjA2MTExNjA3NX0.fWWMBg84zjeaCDAg-DV1SOJwVjbWDzKVsIMUTuVUVsY"
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route("/")
 def index():
